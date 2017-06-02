dfba4c0cd5d9a5c78479e03c14d220649714a2f8
vkalfieri <phantom_11@hotmail.com>
2017-06-02 15:06:38 +1000

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SwapDeck
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
